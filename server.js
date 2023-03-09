27df50325e49e72456ae3363e3fc0ebedbf02993
AMA-Alyssa <102494494+AMA-Alyssa@users.noreply.github.com>
2023-03-08 19:50:35 -0600

    added cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
+const PORT = 8000
 
 let db, 
     dbConnectionString = process.env.DB_STRING,
@@ -20,7 +21,15 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+app.use(cors())
 
+app.get('/', async (request, response) => {
+    try {
+        response.render('index.ejs')
+    } catch (error){
+        response.status(500).send({message: error.message})
+    }
+})
 
 app.listen(process.env.PORT || PORT, () =>{
     console.log(`Server is running`)
